f42a6857c67266af34ed056bcae1a7c095b57766
Ivan Perera <ivan@majestic.com.au>
2019-10-25 10:58:47 +1100

    Search box added and make it as independent component where we can use it later

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import React, { Component } from 'react'; 
-import {CardList} from './components/card-list/card-list.component';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import "./App.css";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
 
-    this.state ={
-      monsters: []
+    this.state = {
+      monsters: [],
+      searchField: ""
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
-  
 
   render() {
+    /**
+     * ====Destructuring assignment===
+     * The following assignment is equivalent to
+     * const monsters = this.state.monsters;
+     * const searchField = this.state.searchField;
+     *
+     * */
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-      <CardList monsters={this.state.monsters}>
-      </CardList>
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
