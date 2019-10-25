cded4353d5ed92d02b7e1748a7d4bdcff2f767aa
Ivan Perera <ivan@majestic.com.au>
2019-10-25 15:40:40 +1100

    Updates

---
+++ precache-manifest.79de573c95186febf1f8f0497ecc95d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb1e3ebb624c64a153f2f2b406c2b01",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "50e3e16096f3c58c0c80",
+    "url": "/monsters-rolodex/static/css/main.37e25fc7.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "50e3e16096f3c58c0c80",
+    "url": "/monsters-rolodex/static/js/main.da32ecc0.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
